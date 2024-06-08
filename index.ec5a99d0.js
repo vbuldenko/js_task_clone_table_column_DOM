69d9d7eeb3b9b3055cfaa1742e2b0836a94b1f05
Vladyslav <vladbuldenko@gmail.com>
2024-06-08 18:02:11 +0300

    make build

---
+++ index.ec5a99d0.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table"),r=!0,l=!1,t=void 0;try{for(var n,o=e.rows[Symbol.iterator]();!(r=(n=o.next()).done);r=!0){var a=n.value,i=a.cells[1].cloneNode(!0);a.lastElementChild.insertAdjacentElement("beforebegin",i)}}catch(e){l=!0,t=e}finally{try{r||null==o.return||o.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.ec5a99d0.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const table = document.querySelector("table");
+for (const row of table.rows){
+    const secondCell = row.cells[1];
+    const clonedCell = secondCell.cloneNode(true);
+    const lastCell = row.lastElementChild;
+    lastCell.insertAdjacentElement("beforebegin", clonedCell);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
